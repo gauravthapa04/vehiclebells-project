6e6d236aa255bdb3f8d8a72dca590c0a3ca1c039
uiwebsoft <uiwebsoft@gmail.com>
2023-05-12 23:04:05 +0530

    Page based Styling

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,6 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import '../src/components/assets/css/dashboard.css';
 import { SessionProvider } from "next-auth/react"
 export default function App({
   Component, pageProps: { session, ...pageProps }
